e3f5f6f3c2ef7ac38738ff4f3f8f2b5bac375d8e
maciejSamerdak <sam.maciej@interia.pl>
2020-03-28 21:55:12 +0100

    Add read/write operations for a single file containing all data

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import calendar
-import datetime
 
 
 def numericdatetostringdate(year, month, day=0):
@@ -19,34 +18,47 @@ def numericdatetostringdate(year, month, day=0):
     return string_date
 
 
-""" Loading data location """
-file = open('data_location.cfg')
-data_location = file.readline()
-data_location = data_location.split(';')[0]
-data_column_names = file.readline().split(';')[:-1]
+def load_data_from_source():
+    file = open('data_location.cfg')
+    data_location = file.readline()
+    data_location = data_location.split(';')[0]
+    data_column_names = file.readline().split(';')[:-1]
+    file.close()
 
-""" Loading data """
-data_dict = {}
-years = [2015, 2016]
-months = [1, 2, 3, 4, 5, 6, 10, 11, 12]
-c = calendar.Calendar()
-for year in years:
-    for month in months:
-        for day in c.itermonthdays(year, month):
-            # print(day)
-            if day:
-                data_file_name = data_location + '/'+numericdatetostringdate(year, month)+'/statystyki-wifi-'+numericdatetostringdate(year, month, day)+'.csv'
-                try:
-                    data_dict[numericdatetostringdate(year, month, day)] = pd.read_csv(filepath_or_buffer=data_file_name, sep=';', index_col=False, header=0, names=data_column_names)
-                    data_dict[numericdatetostringdate(year, month, day)]['dataPomiaru'] = pd.to_datetime(data_dict[numericdatetostringdate(year, month, day)]['dataPomiaru'], format="%Y-%m-%d--%H-%M")
-                except FileNotFoundError:
-                    print(numericdatetostringdate(year, month, day)+" is missing")
+    data_dict = {}
+    years = [2015, 2016]
+    months = [1, 2, 3, 4, 5, 6, 10, 11, 12]
+    c = calendar.Calendar()
+    for year in years:
+        for month in months:
+            for day in c.itermonthdays(year, month):
+                # print(day)
+                if day:
+                    data_file_name = data_location + '/'+numericdatetostringdate(year, month)+'/statystyki-wifi-'+numericdatetostringdate(year, month, day)+'.csv'
+                    try:
+                        data_dict[numericdatetostringdate(year, month, day)] = pd.read_csv(filepath_or_buffer=data_file_name, sep=';', index_col=False, header=0, names=data_column_names)
+                        data_dict[numericdatetostringdate(year, month, day)]['dataPomiaru'] = pd.to_datetime(data_dict[numericdatetostringdate(year, month, day)]['dataPomiaru'], format="%Y-%m-%d--%H-%M")
+                    except FileNotFoundError:
+                        print(numericdatetostringdate(year, month, day)+" is missing")
+    return data_dict
 
-""" Concatenating tables/datafiles """
-data = None
-for data_day in data_dict.values():
-    if data is None:
-        data = data_day
-    else:
-        data.append(data_day, ignore_index=True)
+
+def concatenate_tables(data_dict):
+    data = None
+    for data_day in data_dict.values():
+        if data is None:
+            data = data_day
+        else:
+            data.append(data_day, ignore_index=True)
+    return data
+
+
+try:
+    data = pd.read_csv(filepath_or_buffer='complete_data.csv', index_col=False)
+except FileNotFoundError:
+    data = concatenate_tables(load_data_from_source())
+    data_file = open('complete_data.csv', 'w')
+    data_file.write(data.to_csv(index=False))
+    data_file.close()
 print(data)
+
